c704f21b923e0afe744c395b19aec6dab2811c3f
alexeid <alexeid@iface.ru>
2021-11-30 10:34:54 +0500

    заготовка сборки CI, убираю лишнее

--- build_linux_simple.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Path "dist_linux" | Remove-Item -Force -Recurse -ErrorAction Ignore
-
-dotnet publish -c Release -r linux-x64 --self-contained false -p:PublishSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false -o dist_linux
--- build_windows_simple.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Path "dist" | Remove-Item -Force -Recurse -ErrorAction Ignore
-
-dotnet publish -c Release -r win-x64 --self-contained false -p:PublishSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false -o dist
